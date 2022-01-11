7f4828e4955be5dc781718564bda9e5d1f682faf
Miraethefuture <miraeaeaeaeae@gmail.com>
2022-01-11 15:55:53 +0900

    yo

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,10 +1,4 @@
 var store = [{
-        "title": "개발 공부하며 정리해보는 영단어 목록",
-        "excerpt":"개발 공부를 하며 만난 영단어들을 정리합니다.      개발용어(x) 공부하며 만난 보통 영단어(o)   ","categories": [],
-        "tags": [],
-        "url": "/Sidebar%20with%20Navigation%20List/",
-        "teaser": "/assets/images/teaser.jpg"
-      },{
         "title": "만들어 가면서 배우는 JAVA 플레이그라운드(구구단 구현하기 추가미션)",
         "excerpt":"아래 모든 내용은 인프런 강의 ‘만들어 가면서 배우는 JAVA 플레이그라운드’의 내용입니다. 추가 미션 문제 사용자에게 입력 값을 받아서 구구단을 출력 입력값이 11 이라면 2 * 1 부터 2 * 11 까지 ~ 11 * 1 부터 11 * 11 까지 한번에 출력 현재 상황: 11을 입력해서 11 * 1 ~...","categories": [],
         "tags": ["code","Java"],
@@ -12,8 +6,8 @@ var store = [{
         "teaser": "/assets/images/teaser.jpg"
       },{
         "title": "영단어 정리",
-        "excerpt":"🗃️ A append to add something to a piece of writing. 글에 어떤 내용을 덧붙이다. 덧붙이다 첨부하다 allow to make something happen, especially something helpful or useful. 어떤일이 일어나는 것을 가능하게 만들다. automate to start using computers or machines to do a job, rather than people 어떤 일을 하기 위해...","categories": ["vocabularies","영어단어","vocab"],
-        "tags": ["studying","learning"],
+        "excerpt":"🗃️ A append to add something to a piece of writing. 글에 어떤 내용을 덧붙이다. 덧붙이다 첨부하다 allow to make something happen, especially something helpful or useful. 어떤일이 일어나는 것을 가능하게 만들다. automate to start using computers or machines to do a job, rather than people 어떤 일을 하기 위해...","categories": ["vocabularies","영어단어"],
+        "tags": ["studying","learning","vocab"],
         "url": "/vocab/verb/",
         "teaser": "/assets/images/teaser.jpg"
       },{
@@ -27,5 +21,11 @@ var store = [{
         "excerpt":"오늘의 공부는 Gradle 이 무엇인지 알아보며 시작해본다. Gradle 은 웹 애플리케이션을 만들 때 자동으로 자료들을 모아주고 테스트해주고 빌드해주는 소프트웨어다. ‘a build system’ 또는 ‘a build automation tool’ 이라고 하는데 구글링을 해보니 한국말로도 ‘빌드 도구’ 라고 번역되어 있다. 그렇다면 build tool 은 정확히 무엇일까? Build tools는 작성된 코드를 응용 프로그램으로 만들어주는...","categories": ["TIL"],
         "tags": ["software","learning","공부 기록"],
         "url": "/til/gradle/",
+        "teaser": "/assets/images/teaser.jpg"
+      },{
+        "title": "[20220111] Spring Boot Annotation/REST resources",
+        "excerpt":"Spring Boot Annotation 스프링 부트 애노테이션은 메타 데이터의 한 타입이다. (메타 데이터는 거대한 데이터베이스 속에 무엇이 들어있는지 알려주는 정보인데, 예를 들면 누가 이 정보를 기록했고, 무엇을 위한 정보이고, 어떤 형식으로 저장되었는지를 알려준다.) 애노테이션은 프로그램에 대한 정보를 보충하기 위해 사용되는데 어플리케이션의 일부는 아니다. 작동하는 코드에 직접적인 영향을 미치지 않는다. 컴퓨터가 이해할...","categories": ["TIL"],
+        "tags": ["software","learning","공부 기록"],
+        "url": "/til/annoation/",
         "teaser": "/assets/images/teaser.jpg"
       }]
